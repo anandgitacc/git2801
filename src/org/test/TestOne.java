c14c635a2bccb45fdec6cb9714397036b2e44495
anandgitacc <anand.akmail@gmail.com>
2022-01-28 12:15:44 +0530

    firstgit

---
+++ src/org/test/TestOne.java
@@ -0,0 +1,32 @@
+package org.test;
+
+
+public class TestOne {
+	
+	private void empName() {
+
+		System.out.println("Employee name is : Anand");
+
+	}
+
+	private void empMobNo() {
+
+		System.out.println("Employee mobile number is : 8667070832");
+
+	}
+
+	private void empId() {
+		System.out.println("Employee id is : 6546");
+	}
+
+	public static void main(String[] args) {
+
+		TestOne ed = new TestOne();
+
+		ed.empId();
+		ed.empMobNo();
+		ed.empName();
+
+	}
+
+}
